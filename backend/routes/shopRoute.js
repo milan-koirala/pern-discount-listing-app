2fa7110a7d5cd64817f39a1540eab6ca67864853
Milan Koirala <koiralamilon67@gmail.com>
2025-08-05 19:11:53 +0545

    refactor: split shop update route into info and password

--- backend/routes/shopRoute.js
+++ backend/routes/shopRoute.js
@@ -1,13 +1,20 @@
 import express from 'express';
-import { createShop, deleteShop, getShop, getShops, loginShop, updateShop } from '../controllers/shopController.js';
+import {
+    createShop,
+    deleteShop,
+    getShop,
+    getShops,
+    updateShopInfo,
+    updateShopPassword,
+} from '../controllers/shopController.js';
 
 const router = express.Router();
 
 router.get("/", getShops);
 router.get("/:id", getShop);
 router.post("/register", createShop);
-router.post("/login", loginShop);
-router.put("/:id", updateShop);
+router.put("/:id/info", updateShopInfo);
+router.put("/:id/password", updateShopPassword);
 router.delete("/:id", deleteShop);
 
 export default router;
