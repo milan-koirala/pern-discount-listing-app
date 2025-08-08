7220408c9e04e750bccf65bc0f0f7ef81a56fd16
Milan Koirala <koiralamilon67@gmail.com>
2025-08-08 18:25:20 +0545

    feat(api): add endpoint to fetch discount by ID

--- backend/routes/discountRoute.js
+++ backend/routes/discountRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDiscount, getDiscounts, getDiscountsByShopId  } from "../controllers/discountController.js";
+import { addDiscount, getDiscounts, getDiscountById, getDiscountsByShopId  } from "../controllers/discountController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", verifyToken, getDiscounts);
 router.get("/my", verifyToken, getDiscountsByShopId);
 router.post("/add", verifyToken, addDiscount);
+router.get("/:id", verifyToken, getDiscountById);
+
 
 export default router;
