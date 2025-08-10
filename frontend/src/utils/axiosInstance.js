79c54fc68a037f25f3f27acbfb0fa29e0fdfd617
Milan Koirala <koiralamilon67@gmail.com>
2025-08-10 14:57:55 +0545

    Update base url for deploy

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+// base url will be dynamic depending on the environment
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
+
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
